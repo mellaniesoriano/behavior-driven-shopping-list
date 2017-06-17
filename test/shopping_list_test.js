1ffe24435fb2dc809750f534c2c03cad23cdba93
Seolhee Kim <seolhee@upspringmedia.com>
2017-06-17 08:53:30 -1000

    created render() method test

--- test/shopping_list_test.js
+++ test/shopping_list_test.js
@@ -46,17 +46,16 @@ describe('ShoppingListItem class', function() {
     it('should have a method name render and set a set HTML formatted string', function() {
       expect(goji.render).to.be.a('function');
       expect(goji.render()).to.be.a('string');
+      expect(goji.render()).to.equal(`<li class=completed_${goji.isDone}><span>${goji.name}</span> <span>${goji.description}</span></li>`);
     });
   });//ending shoppinglistitem
 
   describe('ShoppingList Class', function() {
     var shoppingList = ShoppingList;
     var myShoppingList;
-    var cereal;
 
     beforeEach(function() {
       myShoppingList = new ShoppingList();
-      cereal = new ShoppingListItem("cereal", "coco puffs")
     });
 
     it('should be a function', function() {
@@ -76,11 +75,64 @@ describe('ShoppingListItem class', function() {
         expect(myShoppingList.addItem).to.be.a('function')
       });
 
-      it('should add a object to the items array', function () {
-        myShoppingList.addItem(cereal).should.contain(cereal);
+      it('should have a instance of ShoppingListItem', function () {
+        var cereal = new shoppingListItem("Coco Puffs");
+        myShoppingList.addItem(cereal);
+        expect(cereal instanceof ShoppingListItem).to.equal(true);
+      });
+
+      it('should throw error if item does not take an argument', function () {
+        expect(myShoppingList.addItem).to.throw('ERROR');
       });
 
     });
+
+    describe('removeItem() method', function() {
+      it('should have a method name removeItem and have a single ShoppingListItem argument', function() {
+        expect(myShoppingList.removeItem).to.be.a('function')
+      });
+
+      it('should remove the object from the ShoppingList', function() {
+        var cereal = new ShoppingListItem("Coco Puffs");
+        myShoppingList.addItem(cereal)
+        myShoppingList.removeItem(cereal)
+        expect(myShoppingList.items).to.not.include(cereal)
+      });
+
+      it('should remove the last item in the ShoppingList if there are no parameters', function() {
+        var cereal = new ShoppingListItem("Coco Puffs");
+        myShoppingList.addItem(cereal)
+        function removeItem(){
+          expect(undefined).to.be.arguments
+        }
+      });
+
+      it('should throw error if item is not an instanceof ShoppingListItem', function () {
+        expect(myShoppingList.addItem).to.throw('ERROR');
+      });
+    });
+
+    describe('render() method', function() {
+      it('should have a method name render', function() {
+        expect(myShoppingList.render).to.be.a('function')
+      });
+
+      it('should concatenate each items in the array', function() {
+        var cereal = new ShoppingListItem("Coco Puffs");
+        var milk = new ShoppingListItem("low fat milk")
+        myShoppingList.addItem(cereal);
+        myShoppingList.addItem(milk);
+        expect(cereal.items & milk.items).to.be.nested
+      });
+
+      it('should return an HTML formatted string wrapped in a <ul> tags', function(){
+        var cereal = new ShoppingListItem("Coco Puffs");
+        var milk = new ShoppingListItem("low fat milk");
+        expect(myShoppingList.render()).to.be.a('string');
+        expect(myShoppingList.render()).to.equal(`<ul></ul>`)
+      })
+    });
+
 
 
 
